7c90e52711be1dcd258ae7ca112babc3a3db7ba6
Raymond <solarinayomide08@gmail.com>
2022-06-30 19:46:03 -0700

    Add some files

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+$!/bin/bash
+#include <stdio.h>
+int main (void)
+{char var1;
+	int var2 
+;
+	long int var3;
+	long long int var4;
+	float f;
+	printf("Size of a char:%lu byte(s) \n", (unsigned long)sizeof(var1))
+		printf("Size of an int: %lu byte(s)\n (unsigned long)sizeof(var2));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(var3));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(var4)):
+		printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+
+	return (0);
