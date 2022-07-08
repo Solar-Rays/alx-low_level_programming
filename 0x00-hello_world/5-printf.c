2251bb507354d7faee3543ad0de9943cf750927b
Raymond <solarinayomide08@gmail.com>
2022-07-08 16:29:33 -0700

    Make printf betty compliant

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,12 @@
-#!/bin/bash
-#include <stdio.h> 
+#include <stdio.h>
 
-int main (void)
+/**
+ * main - This is the main function of the program
+ *
+ * Return: always returns 0
+ */
+int main(void)
 {
-}printf("with proper grammat, but the outcome is a piece of art"\n);
-return 0;
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
