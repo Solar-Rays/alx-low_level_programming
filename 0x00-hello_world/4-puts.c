26c8a399a21f347f1751cffde27ffa2c85ab5e5c
Raymond <solarinayomide08@gmail.com>
2022-07-08 13:26:20 -0700

    Made puts.c betty compliant

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
-int main (void)
+/**
+ * main - This is the main function
+ *
+ * Return: 0 always for the main function
+*/
+int main(void)
 {
-puts("Programming is like building a multilingual puzzle\n");
-return (0);
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
 }
