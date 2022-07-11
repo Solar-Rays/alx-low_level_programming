1fcc37151fae4b3735e47928eebc43f3ee49cc70
Raymond <solarinayomide08@gmail.com>
2022-07-11 01:48:42 -0700

    Add quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+* main - main function of the program
+*
+* Return: (1) in this instance.
+*/
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
