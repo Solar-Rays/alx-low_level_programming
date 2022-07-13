849e3c26aedbdeeb1c0cc361afa3e090bc35177d
Raymond <solarinayomide08@gmail.com>
2022-07-13 16:03:09 -0700

    Add 101 print comb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - The primary function in the program
+ *
+ * Return: Always 0
+ *
+ */
+int main(void)
+{
+	int a, b, c;
+
+	for (a = 48; a < 58; a++)
+	{
+		for (b = 49; b < 58; b++)
+		{
+			for (c = 50; c < 58; c++)
+			{
+				if (c > b && b > a)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(c);
+					if (a != 55 || b != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
