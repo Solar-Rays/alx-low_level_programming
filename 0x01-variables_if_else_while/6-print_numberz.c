014b8a3f03c70d09264b9ade3b090e1f4991c171
Raymond <solarinayomide08@gmail.com>
2022-07-13 14:10:33 -0700

    Add print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - The primary function of this program
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int alpha;
+
+	for (alpha = 48; alpha <= 57; alpha++)
+	{
+		putchar(alpha);
+	}
+	putchar(10); /*New line*/
+
+	return (0);
+}
