ad1085fd0fb620ee714426d1cfa4071ebdfc244c
Raymond <solarinayomide08@gmail.com>
2022-07-13 05:58:27 -0700

    Make 0-positive_or_negative betty compliant

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h> 
+#include <time.h>
 
 /**
  * main - This is the primary function in this program.
@@ -14,15 +14,18 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	if(n > 0) {
+	if (n > 0)
+	{
 		printf("%d is positive\n", n);
-	} 
+	}
 
-	else if(n == 0) {
+	else if (n == 0)
+	{
 		printf("%d is zero\n", n);
 	}
 
-	else {
+	else
+	{
 		printf("%d is negative\n", n);
 	}
 
