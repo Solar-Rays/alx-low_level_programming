be9454a70bf2d4ecc5855ce9e92bd9f4b5cd1141
Raymond <solarinayomide08@gmail.com>
2022-07-25 23:14:50 +0000

    Add c files

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _islower - this is the main function of the program
+ *
+ * @c: a char
+ * Return: returns 1 or 0
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - Primary function of the program
+ * @n: parameter
+ * Return: absolute number
+ *
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		n = -n;
+	return (n);
+}
